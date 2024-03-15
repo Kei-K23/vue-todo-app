dc0b9eddbdd4a37c9baa313f1bd2334a011f3c4e
Kei-K23 <keiksl2301@gmail.com>
2024-03-16 01:13:11 +0630

    add form to create new task

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from "vue";
+import Form from "./components/Form.vue";
+
+const tasks = ref([]);
+
+function addTask(task) {
+  tasks.value.push(task);
+}
+</script>
+
+<template>
+  <main>
+    <h1 class="mt-4 italic font-bold text-xl text-center">
+      Write your awesome ideas down
+    </h1>
+    <Form @add-task="(t) => addTask(t)" />
+    <ul>
+      <li v-for="task in tasks" :key="task.id">
+        {{ task.name }}
+      </li>
+    </ul>
+  </main>
+</template>
---
+++ src/components/Form.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from "vue";
+
+let id = 0;
+const emit = defineEmits(["addTask"]);
+
+const task = ref();
+
+function onSubmit() {
+  emit("addTask", {
+    id: id++,
+    name: task.value,
+    completed: false,
+    createdAt: new Date().getTime(),
+  });
+
+  task.value = "";
+}
+</script>
+
+<template>
+  <form @submit.prevent="onSubmit">
+    <input
+      type="text"
+      v-model="task"
+      placeholder="What needs to be done?"
+      class="input input-bordered w-full max-w-xs"
+    />
+  </form>
+</template>
